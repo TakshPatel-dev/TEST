499ffb6dbae14fdb00731b6ac5ee60ceb371511d
TakshPatel <pateltaksh20061022@gmail.com>
2024-06-18 10:59:50 +0530

    sdfsdf

--- rand.js
+++ rand.js
@@ -1,6 +1,6 @@
 var VUE = ""
  VUE += `<button class="btn btn-moving-gradient btn-moving-gradient--pink" id="button"> Roll A Number </button>
     <br>
-    <h2 id="Num">klkl</h2>`
+    <h2 id="Num">klklsdfdsfsd</h2>`
 console.log(VUE)
 document.body.innerHTML = VUE
